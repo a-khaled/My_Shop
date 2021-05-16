7837f1bf0eda9d721a3013dd8a57d6fc2fbc9a37
Khaled <a.khaledshehab@gmail.com>
2021-05-16 20:28:33 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import './screens/products_overview_screen.dart';
+import './screens/product_detail_screen.dart';
+import './providers/products.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => Products(),
+      child: MaterialApp(
+        title: 'MyShop',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          accentColor: Colors.redAccent,
+          fontFamily: 'Lato',
+        ),
+        home: ProductsOverviewScreen(),
+        debugShowCheckedModeBanner: false,
+        routes: {
+          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+        },
+      ),
+    );
+  }
+}
+
