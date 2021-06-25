681840ad866ad13475dbc5b93d7a54f74e97e833
Khaled <a.khaledshehab@gmail.com>
2021-06-25 15:36:30 +0200

    added animations

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ import './screens/edit_product_screen.dart';
 import 'package:my_shop/screens/auth_screen.dart';
 import 'providers/auth.dart';
 import 'screens/splash_screen.dart';
+import 'helpers/custom_route.dart';
 
 void main() {
   runApp(MyApp());
@@ -47,6 +48,10 @@ class MyApp extends StatelessWidget {
             primarySwatch: Colors.blue,
             accentColor: Colors.redAccent,
             fontFamily: 'Lato',
+            pageTransitionsTheme: PageTransitionsTheme(builders: {
+              TargetPlatform.android: CustomPageTransitionBuilder(),
+              TargetPlatform.iOS: CustomPageTransitionBuilder(),
+            }),
           ),
           home: auth.isAuth
               ? ProductsOverviewScreen()
